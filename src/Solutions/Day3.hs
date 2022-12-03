b5b926056050682884098803d211e476a617b6cb
sabbey95 <samabbey109@yahoo.co.uk>
2022-12-03 08:25:11 +0000

    day 3

--- src/Solutions/Day3.hs
+++ src/Solutions/Day3.hs
@@ -3,19 +3,51 @@ module Solutions.Day3
   ) where
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
+                                      printSolutions, printTestSolutions)
 import           Text.Trifecta       (Parser)
+import Text.Parser.Combinators
+import Text.Parser.Token (token)
+import Text.Parser.Char (letter)
+import Data.Bifunctor (Bifunctor(bimap))
+import Data.List (elemIndex, intersect)
+import Data.Maybe (catMaybes, mapMaybe)
+import Common.Debugging (traceLns)
 
 aoc3 :: IO ()
 aoc3 = do
   printSolutions 3 $ MkAoCSolution parseInput part1
   printSolutions 3 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+parseInput :: Parser [String]
+parseInput = do
+  some $ token $ some letter
 
-part1 :: String -> String
-part1 = undefined
+part1 :: [String] -> Int
+part1 = sum . mapMaybe calculateScore . mapMaybe findDuplicate
 
-part2 :: String -> String
-part2 = undefined
+findDuplicate:: String -> Maybe Char
+findDuplicate s
+  | null common = Nothing
+  | otherwise = Just $ head common
+  where (first, second) = splitString s
+        common = first `intersect` second
+
+calculateScore:: Char -> Maybe Int
+calculateScore c = do
+  i <- elemIndex c (['a'..'z'] ++ ['A'..'Z'])
+  pure $ i + 1
+
+splitString:: String -> (String, String)
+splitString [] = ("", "")
+splitString [x] = ([x], "")
+splitString (x:x1) = bimap (x :) (last x1 :) rest
+    where rest = splitString $ init x1
+
+part2 :: [String] -> Int
+part2 = sum . mapMaybe calculateScore . findItemTypes
+
+findItemTypes:: [String] -> [Char]
+findItemTypes s 
+  | length s <= 3 = [head $ findBadge s]
+  | otherwise = head (findBadge (take 3 s)) : findItemTypes (drop 3 s)
+  where findBadge x = foldr intersect (head x) x
