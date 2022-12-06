c1297bdb10078c35f9524335b5a46f2ece15dcbd
sabbey95 <samabbey109@yahoo.co.uk>
2022-12-06 08:52:44 +0000

    Day 6

--- src/Solutions/Day6.hs
+++ src/Solutions/Day6.hs
@@ -1,10 +1,12 @@
 module Solutions.Day6
-  ( aoc6
-  ) where
+    ( aoc6
+    ) where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import           Common.AoCSolutions     (AoCSolution (MkAoCSolution),
+                                          printSolutions)
+import qualified Data.Set                as S
+import           Text.Parser.Combinators (some)
+import           Text.Trifecta           (CharParsing (anyChar), Parser)
 
 aoc6 :: IO ()
 aoc6 = do
@@ -12,10 +14,21 @@ aoc6 = do
   printSolutions 6 $ MkAoCSolution parseInput part2
 
 parseInput :: Parser String
-parseInput = undefined
+parseInput = do
+  some anyChar
 
-part1 :: String -> String
-part1 = undefined
+part1 :: String -> Int
+part1 = solve 4
 
-part2 :: String -> String
-part2 = undefined
+solve::  Int -> String -> Int
+solve = checkPacket 0
+
+part2 :: String -> Int
+part2 = solve 14
+
+checkPacket:: Int -> Int -> String -> Int
+checkPacket index messageLength str
+  | length str < messageLength = index
+  | otherwise =  if S.size (S.fromList (take messageLength str)) == messageLength 
+                  then index + messageLength 
+                  else checkPacket (index+1) messageLength (tail str)
