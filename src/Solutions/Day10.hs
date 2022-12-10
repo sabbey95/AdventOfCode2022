624885c129f7d562182cabb768ba84867549f49b
sabbey-skedulo <sam.abbey@skedulo.com>
2022-12-10 10:31:34 +0000

    Day 10

--- src/Solutions/Day10.hs
+++ src/Solutions/Day10.hs
@@ -1,21 +1,60 @@
 module Solutions.Day10
-  ( aoc10
-  ) where
+    ( aoc10
+    ) where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import           Common.AoCSolutions     (AoCSolution (MkAoCSolution),
+                                          printSolutions, printTestSolutions)
+import           Control.Applicative
+import           Text.Parser.Char        (string)
+import           Text.Parser.Combinators (some, try)
+import           Text.Parser.Token       (token)
+import           Text.Trifecta           (Parser, integer, newline)
+import Common.Debugging (traceLns)
+import Control.Monad (join)
+import Data.List.Split (chunksOf)
 
 aoc10 :: IO ()
 aoc10 = do
   printSolutions 10 $ MkAoCSolution parseInput part1
   printSolutions 10 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+type NoopCommand = String
+type AddXCommand = Integer
+data Command
+  = AddXCommand Integer
+  | NoopCommand String
+  deriving (Show)
 
-part1 :: String -> String
-part1 = undefined
+parseInput :: Parser [Command]
+parseInput = do
+  some $ token (try (AddXCommand <$> parseAddX) <|> try (NoopCommand <$> parseNoop))
+  where parseNoop:: Parser NoopCommand
+        parseNoop = do
+          string "noop"
+        parseAddX:: Parser AddXCommand
+        parseAddX =  do
+          string "addx "
+          integer
 
-part2 :: String -> String
-part2 = undefined
+part1 :: [Command] -> Int
+part1 = sum . toSignalStrengths [20, 60,100,140,180,220] . calculateRegister
+
+toSignalStrengths :: [Int] -> [Integer] -> [Int]
+toSignalStrengths requiredCylces allCycles = map (\c -> c * fromIntegral (allCycles!!c)) requiredCylces
+
+calculateRegister:: [Command] -> [Integer]
+calculateRegister = foldl performCommand [1]
+
+performCommand :: [Integer] -> Command -> [Integer]
+performCommand cycles (NoopCommand _) = cycles ++ [last cycles]
+performCommand cycles (AddXCommand x) = cycles ++ [lastCycle, lastCycle + x]
+  where lastCycle = last cycles
+
+
+part2 :: [Command] -> String
+part2 = unlines . findLitPixels . calculateRegister
+
+findLitPixels:: [Integer] -> [String]
+findLitPixels register = map (zipWith (curry isLit) [0..]) $ chunksOf 40 register
+  where isLit :: (Int, Integer) -> Char
+        isLit (pos, reg) = if abs(toInteger pos - reg) <=1 then '#' else '.'
