0620883da8e15c4f46fed070ddb446c94e87f406
sabbey-skedulo <sam.abbey@skedulo.com>
2022-12-13 12:27:02 +0000

    Day 13 + 14

--- src/Solutions/Day12.hs
+++ src/Solutions/Day12.hs
@@ -3,19 +3,92 @@ module Solutions.Day12
   ) where
 
 import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+                                      printSolutions, printTestSolutions)
+import           Text.Trifecta       (Parser, letter)
+import qualified Data.Map as M
+import Text.Parser.Combinators (some)
+import Text.Parser.Token (token)
+import Data.Foldable (find)
+import qualified Control.Lens as Data.Bifunctor
+import Data.Maybe (mapMaybe, fromMaybe)
+import Common.Debugging (traceLns)
+import Debug.Trace (trace)
+import Data.List.Split (chunksOf)
 
 aoc12 :: IO ()
 aoc12 = do
   printSolutions 12 $ MkAoCSolution parseInput part1
   printSolutions 12 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+type Point = (Int, Int)
+type HeightMap = M.Map Point Char
+type WeightMap = M.Map Point Int
 
-part1 :: String -> String
-part1 = undefined
+parseInput :: Parser HeightMap
+parseInput = do
+  lines <- some $ token $ some letter
+  pure $ mapLines lines
 
-part2 :: String -> String
-part2 = undefined
+mapLines :: [[Char]] -> HeightMap
+mapLines lines = foldr mapLine M.empty numberedLines
+    where numberedLines = zip lines [0..]
+
+mapLine :: ([Char], Int) -> HeightMap -> HeightMap
+mapLine (line, row) currentHeights = foldr insertPoint currentHeights numberedChars
+    where numberedChars = zip line [0..]
+          insertPoint (char, column) = M.insert (column, row) char
+
+part1 :: HeightMap -> Maybe Int
+part1 x = M.lookup start $ solve x
+  where start = head $ lookupPoint 'S' x
+
+solve ::  HeightMap -> WeightMap
+solve x =  findShortestPath x start end $ M.fromList [(end, 0)]
+  where start = head $ lookupPoint 'S' x
+        end = head $ lookupPoint 'E' x
+
+
+findShortestPath :: HeightMap -> Point -> Point -> WeightMap -> WeightMap
+findShortestPath heightMap destination current weights
+  | destination == current = weights
+  | null realLowerPoints = weights
+  | otherwise = foldr (findShortestPath heightMap destination) updatedWeightMap realLowerPoints
+    where adjacentPoints = map (Data.Bifunctor.bimap (fst current +) (snd current +))[(1,0), (0,1), (-1, 0), (0, -1)]
+          findHeight p = do
+            height <- M.lookup p heightMap
+            Just (height, p)
+          currentWeight = M.findWithDefault 0 current weights
+          lowerPoints = do
+            currentHeight <- M.lookup current heightMap
+            adjacentHeights <- Just (mapMaybe findHeight adjacentPoints)
+            Just . map snd . filter (isTraversable currentHeight) $ adjacentHeights
+          realLowerPoints = filter (fromMaybe True . isWeightReduction currentWeight weights) $ fromMaybe [] lowerPoints
+          updatedWeightMap = addWeights currentWeight weights realLowerPoints
+
+isWeightReduction :: Int -> WeightMap -> Point -> Maybe Bool
+isWeightReduction weight weights p = do
+    pointWeight <- M.lookup p weights
+    Just $ pointWeight > weight + 1
+
+
+addWeights :: Int -> WeightMap -> [Point] -> WeightMap
+addWeights current = foldr (update current)
+
+update :: Int -> Point -> WeightMap -> WeightMap
+update i p = M.insertWith min p (i + 1)
+
+isTraversable :: Char -> (Char, Point) -> Bool
+isTraversable destination (current, _) = fromMaybe False result
+  where charToHeight = M.fromList (zip ['a'..'z'] [1..] ++ [('S', 1), ('E', 26)])
+        result = do
+          destHeight <- M.lookup destination charToHeight
+          currentHeight <- M.lookup current charToHeight
+          Just $ destHeight <= (currentHeight + 1)
+
+lookupPoint :: Char -> HeightMap ->  [Point]
+lookupPoint c m =  map fst . filter (\(_, char) -> char == c) $ M.assocs m
+
+part2 :: HeightMap -> Int
+part2 x = minimum $ mapMaybe (`M.lookup` weights) starts
+  where starts = lookupPoint 'a' x
+        weights = solve x
--- src/Solutions/Day13.hs
+++ src/Solutions/Day13.hs
@@ -1,21 +1,101 @@
 module Solutions.Day13
-  ( aoc13
-  ) where
+    ( aoc13
+    ) where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import           Common.AoCSolutions            (AoCSolution (MkAoCSolution),
+                                                 printSolutions,
+                                                 printTestSolutions)
+import           Control.Applicative            ((<|>))
+import           Data.Function                  (on)
+import           Data.List                      (sortBy, sortOn)
+import           Data.List.NonEmpty             (sortWith)
+import           Data.Maybe                     (fromMaybe, isNothing)
+import           Data.Ord                       (comparing)
+import           Data.Text.Internal.Fusion.Size (compareSize)
+import           Text.Parser.Combinators        (count, many, some, try)
+import           Text.Parser.Token              (token)
+import           Text.Trifecta                  (CharParsing (char), Parser,
+                                                 commaSep, integer, newline,
+                                                 optional)
 
 aoc13 :: IO ()
 aoc13 = do
   printSolutions 13 $ MkAoCSolution parseInput part1
-  printSolutions 13 $ MkAoCSolution parseInput part2
+  printSolutions 13 $ MkAoCSolution parseInputPart2 part2
 
-parseInput :: Parser String
-parseInput = undefined
+type IntNode = Integer
+data Node
+  = IntNode IntNode
+  | Packet [Node]
+  deriving (Eq, Show)
+type Packet = [Node]
+type PacketPair = (Packet, Packet)
 
-part1 :: String -> String
-part1 = undefined
+parseInput :: Parser [PacketPair]
+parseInput = do
+  some $ token parsePacketPair
 
-part2 :: String -> String
-part2 = undefined
+parseInputPart2 :: Parser [Packet]
+parseInputPart2 = do
+  some $ token parsePacketPart2
+  where
+    parsePacketPart2 = do
+      packet <- parsePacket
+      optional newline
+      optional newline
+      pure packet
+
+parsePacketPair :: Parser PacketPair
+parsePacketPair = do
+  first <- parsePacket
+  newline
+  second <- parsePacket
+  optional $ count 2 newline
+  pure (first, second)
+
+parsePacket :: Parser Packet
+parsePacket = do
+  char '['
+  nodes <- many parseNode
+  char ']'
+  pure nodes
+
+parseNode :: Parser Node
+parseNode = do
+  node <- try (Packet <$> parsePacket) <|> try (IntNode <$> integer)
+  optional $ char ','
+  pure node
+
+
+part1 :: [PacketPair] -> Int
+part1 = sum . map fst . filter (\(_, (l, r)) -> comparePacketsWithDefault l r) . zip [1..]
+
+comparePacketsWithDefault:: Packet -> Packet -> Bool
+comparePacketsWithDefault l r = fromMaybe False $ comparePackets l r
+
+comparePackets:: Packet -> Packet -> Maybe Bool
+comparePackets [] [] = Nothing
+comparePackets [] _ = Just True
+comparePackets _ [] = Just False
+comparePackets (left:otherLeft) (right:otherRight) |
+  isNothing nodeComparison = comparePackets otherLeft otherRight
+  | otherwise = nodeComparison
+  where nodeComparison =  compareNodes left right
+
+compareNodes :: Node -> Node -> Maybe Bool
+compareNodes (IntNode l) (IntNode r) = if l == r then Nothing else Just $ l < r
+compareNodes (Packet l) (Packet r)   = comparePackets l r
+compareNodes l (Packet r)            = comparePackets [l] r
+compareNodes (Packet l) r            = comparePackets l [r]
+
+part2 :: [Packet] -> Int
+part2 packets = product .map fst .filter (\(_, p) -> p `elem` dividers) $ zip [1..] sortedPackets
+  where allPackets = packets ++ dividers
+        sortedPackets = sortBy comparator allPackets
+
+comparator :: Packet -> Packet -> Ordering
+comparator l r = if correctOrder then LT else GT
+  where correctOrder = comparePacketsWithDefault l r
+
+dividers:: [Packet]
+dividers = [[Packet [IntNode 2]], [Packet [IntNode 6]]]
